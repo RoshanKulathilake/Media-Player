eda979f4e8ec6928748a080e3b07896963f80904
Roshan Kulathilake <roshan.kula@gmail.com>
2017-10-08 15:28:05 +0530

    MediaPlayer
    Media player is developed by using Java

---
+++ src/amedia/Amedia.java
@@ -0,0 +1,42 @@
+package amedia;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author DK_Roshan
+ */
+public class Amedia extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        stage.setTitle("MEDIA");
+        scene.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent doubleClicked) {
+                if(doubleClicked.getClickCount()==2){
+                    stage.setFullScreen(true);
+                }
+            }
+        });
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
